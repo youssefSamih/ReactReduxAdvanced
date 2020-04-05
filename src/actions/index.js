3ab8501d63faf0146c5ab020714c350f1a955fd2
youssefwork <youssef.samih97@gmail.com>
2020-04-05 03:30:18 +0100

    finish fetching comment from remote api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
-import { SAVE_COMMENT } from "actions/types";
+import Axios from "axios";
+import { SAVE_COMMENT, FETCH_COMMENTS } from "actions/types";
 
-export const saveComment = (comment) => {
+export function saveComment(comment){
   return {
     type: SAVE_COMMENT,
     payload: comment
+  }
+}
+
+export function fetchComments(){
+  const response = Axios.get('http://jsonplaceholder.typicode.com/comments');
+  return {
+    type: FETCH_COMMENTS,
+    payload: response
   }
 }
