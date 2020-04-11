7536fb68ae3c7c1ce1deeacf9ec2c9a74f2e7f56
youssefwork <youssef.samih97@gmail.com>
2020-04-11 21:56:20 +0100

    finish adding header

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -11,6 +11,7 @@ class CommentList extends Component {
   render(){
     return (
       <div>
+        <h1>Comment List</h1>
         <ul>
           {this.renderComments()}
         </ul>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import commentReducer from 'reducers/comment';
+import authReducer from "reducers/auth";
 
 export default combineReducers({
-  comment: commentReducer
-})
+  comment: commentReducer,
+  auth: authReducer
+});
